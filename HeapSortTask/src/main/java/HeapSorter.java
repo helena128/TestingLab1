8f9451c645be6c4bfe9ee5f086b98daf457bc686
Elena Cheprasova <hellogbel@gmail.com>
2018-03-13 10:46:24 +0300

    printArray function implemented

---
+++ HeapSortTask/src/main/java/HeapSorter.java
@@ -0,0 +1,17 @@
+package main.java;
+
+public class HeapSorter {
+    public void sort(int[] arr) {
+
+    }
+
+    private void heapify(int[] arr, int n, int i) {
+
+    }
+
+    private static void printArray(int[] arr) {
+        for (int a : arr)
+            System.out.print(a + "\t");
+        System.out.println();
+    }
+}
