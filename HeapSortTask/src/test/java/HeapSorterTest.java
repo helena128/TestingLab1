028cc19ef6ae03cf930dd6b93600d215c7ab4e01
Elena Cheprasova <hellogbel@gmail.com>
2018-03-18 23:19:04 +0300

    Updated test class

--- HeapSortTask/src/test/java/HeapSorterTest.java
+++ HeapSortTask/src/test/java/HeapSorterTest.java
@@ -31,16 +31,23 @@ public class HeapSorterTest {
 
     @Test
     public void testSort3() {
-        for (int i = 0; i < NUM_TESTS; i++) {
-            int numOfElem = getNumOfElements();
-            int[] arr = createRandomArray(numOfElem);
-            tester(arr);
-        }
+        int[] arr = {906, 213, 327, 219, 804, 527, 226, 508, 298, 28, 130,
+            150, 237, 985, 427, 89, 306, 403, 534, 981, 343, 856, 253, 997, 778,
+            596, 886, 951, 121, 988, 541};
+        assertTrue(tester(arr));
+    }
+
+    @Test
+    public void testSort4() {
+        int[] arr = {754, 510, 334, 432, 732, 88, 952, 880, 215, 812, 189,
+            434, 80, 407, 439, 237, 731, 481, 630, 737, 976, 69, 397, 215,
+            122, 145, 628, 748, 612, 150, 905};
+        assertTrue(tester(arr));
     }
 
     private boolean tester(int[] arr) {
-        HeapSorter sorter = new HeapSorter();
-        int[] mySort = sorter.sort(arr);
+        HeapSorter sorter = new HeapSorter(arr);
+        int[] mySort = sorter.sort();
         Arrays.sort(arr);
         return (mySort == arr);
     }
@@ -54,6 +61,12 @@ public class HeapSorterTest {
         }
         return arr;
     }
+
+    // for debugging
+    /*private void dummyTester(int[] arr) {
+        HeapSorter heapSorter = new HeapSorter(arr);
+        heapSorter.sort();
+    }*/
 
     private int getNumOfElements() {
         return new Random().nextInt(NUM_OF_ELEMENTS_BOUNDRY);
