86d71c6cb7e553f72cb1b74eec848076e33f7836
Elena Cheprasova <hellogbel@gmail.com>
2018-03-18 23:14:11 +0300

    Heap sort using maxHeap implemented

--- HeapSortTask/src/main/java/HeapSorter.java
+++ HeapSortTask/src/main/java/HeapSorter.java
@@ -1,57 +1,111 @@
 package main.java;
 
+
 public class HeapSorter {
-    public int[] sort(int[] arr) {
-        if (arr == null || arr.length < 1)
-            throw new IllegalArgumentException("Array is null or its size less than 1");
+    private static final int MAX_CHARS = 80;
 
-        int n = arr.length;
+    private int[] arr;
+    private int n;
+    private int step;
 
-        // Build heap (rearrange array)
-        for (int i = n / 2 - 1; i >= 0; i--)
-            heapify(arr, n, i);
+    public HeapSorter(int[] arr) {
+        this.arr = arr;
+        if (arr != null) {
+            n = arr.length;
+            step = 0;
+        } else throw new IllegalArgumentException("Array is null or its size is less than 1");
+    }
 
-        // One by one extract an element from heap
-        for (int i = n - 1; i >= 0; i--) {
-            // Move current root to end
-            int temp = arr[0];
-            arr[0] = arr[i];
-            arr[i] = temp;
+    public int[] sort() {
+        System.out.println("Original array: ");
+        printArray();
 
-            // call max heapify on the reduced heap
-            heapify(arr, i, 0);
-        }
+        //printTree();
+        sortTree();
 
         return arr;
     }
 
-    private void heapify(int[] arr, int n, int i) {
-        int largest = i;  // Initialize largest as root
-        int l = 2*i + 1;  // left = 2*i + 1
-        int r = 2*i + 2;  // right = 2*i + 2
+    private void sortTree() {
+        buildHeap();
+    }
 
-        // If left child is larger than root
-        if (l < n && arr[l] > arr[largest])
-            largest = l;
-
-        // If right child is larger than largest so far
-        if (r < n && arr[r] > arr[largest])
-            largest = r;
-
-        // If largest is not root
-        if (largest != i) {
-            int swap = arr[i];
-            arr[i] = arr[largest];
-            arr[largest] = swap;
-
-            // Recursively heapify the affected sub-tree
-            heapify(arr, n, largest);
+    private void buildHeap() {
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(i);
         }
     }
 
-    private static void printArray(int[] arr) {
+    private void heapify(int idx) {
+        /*int childIdx = 2 * parentIdx;
+        while (childIdx <= last) {
+            if (childIdx + 1 <= last && arr[childIdx + 1] > arr[childIdx])
+                childIdx++;
+            if (arr[childIdx] > arr[parentIdx])
+                swap(childIdx, parentIdx);
+            parentIdx = childIdx;
+            childIdx = 2 * parentIdx;
+        }*/
+        step++;
+        System.out.println("Step: " + step);
+        int largest = idx;
+        int leftIndex = 2 * idx + 1;
+        int rightIndex = 2 * idx + 2;
+
+        if (leftIndex < n && arr[idx] < arr[leftIndex]) {
+            largest = leftIndex;
+        }
+        if (rightIndex < n && arr[largest] < arr[rightIndex]) {
+            largest = rightIndex;
+        }
+
+        if (largest != idx) {
+            swap(idx, largest);
+            System.out.println("\tSwapped: " + arr[idx] + " and " + arr[largest]);
+            heapify(largest);
+        } else System.out.println("\tNo swap");
+    }
+
+    private void swap(int idx1, int idx2) {
+        int tmp = arr[idx1];
+        arr[idx1] = arr[idx2];
+        arr[idx2] = tmp;
+    }
+
+    private void printArray() {
         for (int a : arr)
             System.out.print(a + "\t");
         System.out.println();
+    }
+
+    private void printTree() {
+        StringAlignUtils utils;
+        int j;
+        for (int i = 0; i < arr.length; i += j) {
+            j = i + 1;
+            utils = new StringAlignUtils(MAX_CHARS, StringAlignUtils.Alignment.CENTER);
+            int[] nodes = new int[j];
+            //System.out.println("i: " + i + "\tj: " + j);
+
+            System.out.print(utils.format(arrToString(nodes, '\t')));
+        }
+        /*for (int i = 0; i < n; i ++) {
+            int[] tmp = tree[i];
+            //System.out.println("i = " + i);
+            if (tree[i] != null) {
+                for (int j = 0; j < tmp.length; j++) {
+                    //System.out.println("j = " + j);
+                }
+                System.out.println();
+            }
+        }*/
+    }
+
+    private static String arrToString(int[] arr, char delimiter) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            stringBuilder.append(arr[i]).append(delimiter);
+        }
+        return stringBuilder.append('\n').toString();
     }
 }
