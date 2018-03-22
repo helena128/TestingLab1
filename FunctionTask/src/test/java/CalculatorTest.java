02deb3a881efb4b04f91a5e54bf834a8a44d3d04
Elena Cheprasova <hellogbel@gmail.com>
2018-03-23 00:46:06 +0300

    Updated tests for task1

--- FunctionTask/src/test/java/CalculatorTest.java
+++ FunctionTask/src/test/java/CalculatorTest.java
@@ -22,22 +22,67 @@ public class CalculatorTest {
     @Test
     public void testCalcArcCos1() {
         test = new Calculator();
-        assertEquals(test.calculateArcCos(-1), acos(-1), EPS);
+        assertEquals(test.calculateArcCos(-1), acos(-1), EPS); //x=-1
     }
 
     @Test
     public void testCalcArcCos2() {
-        assertEquals(test.calculateArcCos(-sqrt(3) / 2), acos(-sqrt(3) / 2), EPS);
+        assertEquals(test.calculateArcCos(0), acos(0), EPS); //x=0
     }
 
     @Test
     public void testCalcArcCos3() {
-        assertEquals(acos(0), test.calculateArcCos(0), EPS);
+        assertEquals(acos(1), test.calculateArcCos(1), EPS); //x=1
     }
 
     @Test
     public void testCalcArcCos4() {
-        assertEquals(test.calculateArcCos(1.0), acos(1.0), EPS);
+        assertEquals(test.calculateArcCos(-0.5), acos(-0.5), EPS); //-1<x<0
+    }
+
+    @Test
+    public void testCalcArcCos5() {
+        assertEquals(test.calculateArcCos(-sqrt(3) / 2), acos(-sqrt(3) / 2), EPS); //-1<x<0
+    }
+
+    @Test
+    public void testCalcArcCos6() {
+        assertEquals(test.calculateArcCos(sqrt(3) / 2), acos(sqrt(3) / 2), EPS); //0<x<1
+    }
+
+    @Test
+    public void testCalcArcCos7() {
+        assertEquals(test.calculateArcCos(-0.5), acos(-0.5), EPS); //0<x<1
+    }
+
+    @Test
+    public void testCalcArcCos8() {
+        try {
+            assertEquals(test.calculateArcCos(-2), acos(-2), EPS); //x<-1
+        }
+        catch(IllegalArgumentException e){}
+    }
+
+    @Test
+    public void testCalcArcCos9() {
+        try {
+            assertEquals(test.calculateArcCos(-3), acos(-3), EPS); //x<-1
+
+        }
+        catch(IllegalArgumentException e){}
+    }
+
+    @Test
+    public void testCalcArcCos10() {
+        try {
+            assertEquals(test.calculateArcCos(3), acos(3), EPS); //x>1
+        }
+        catch(IllegalArgumentException e){}
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCalcArcCos11() {
+     assertEquals(test.calculateArcCos(2), acos(2), EPS); //x>1
     }
 
 
